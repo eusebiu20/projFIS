13360981aed0f5cb29a6146616589f971cdd6f6b
Eusebiu Sircuta <eusebiusircuta@gmail.com>
2021-04-07 17:58:01 +0300

    up da

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World da da commit");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
